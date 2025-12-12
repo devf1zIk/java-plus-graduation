4f4aaaefaa41e09bbbc6fec29de56a69bf7f4fa1
f1zik <tkyskii2004@gmail.com>
2025-12-12 23:32:44 +0500

    Фикс#28

--- core/request-service/src/main/java/ru/yandex/practicum/controller/RequestAdminController.java
+++ core/request-service/src/main/java/ru/yandex/practicum/controller/RequestAdminController.java
@@ -1,10 +1,10 @@
 package ru.yandex.practicum.controller;
 
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.service.RequestService;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/admin/requests")
@@ -13,8 +13,8 @@ public class RequestAdminController {
 
     private final RequestService requestService;
 
-    @GetMapping("/count/{eventId}")
-    public Long count(@PathVariable @NonNull Long eventId){
-        return requestService.getConfirmedRequestsCountForEvents(List.of(eventId)).get(eventId);
+    @GetMapping("/count")
+    public Map<Long, Long> getConfirmedRequestsCount(@RequestParam("eventIds") List<Long> eventIds) {
+        return requestService.getConfirmedRequestsCountForEvents(eventIds);
     }
 }
