b696e26f93c9c11a1096e50210e08ae4e424cdb4
f1zik <tkyskii2004@gmail.com>
2025-12-12 23:00:20 +0500

    Фикс#27

--- core/interaction-api/src/main/java/ru/yandex/practicum/client/request/RequestOperations.java
+++ core/interaction-api/src/main/java/ru/yandex/practicum/client/request/RequestOperations.java
@@ -2,14 +2,15 @@ package ru.yandex.practicum.client.request;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.yandex.practicum.dto.request.RequestDto;
 import java.util.List;
 import java.util.Map;
 
 public interface RequestOperations {
 
-    @GetMapping("/admin/requests/count/{eventId}")
-    Map<Long, Long> getConfirmedRequestsCount(@PathVariable List<Long> eventIds);
+    @GetMapping("/admin/requests/count")
+    Map<Long, Long> getConfirmedRequestsCount(@RequestParam("eventIds") List<Long> eventIds);
 
     @GetMapping("/users/{userId}/events/{eventId}/requests")
     List<RequestDto> getEventRequests(@PathVariable Long userId,
