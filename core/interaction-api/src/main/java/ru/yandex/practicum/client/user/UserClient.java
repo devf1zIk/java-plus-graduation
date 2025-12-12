7f9ee2fa5195d4527dafc1d5b51365bdeade177b
f1zik <tkyskii2004@gmail.com>
2025-12-12 13:51:05 +0500

    Фикс#14

--- core/interaction-api/src/main/java/ru/yandex/practicum/client/user/UserClient.java
+++ core/interaction-api/src/main/java/ru/yandex/practicum/client/user/UserClient.java
@@ -1,12 +1,8 @@
 package ru.yandex.practicum.client.user;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.dto.user.UserShortDto;
 
-@FeignClient(name = "user-service", path = "/admin/users", contextId = "userAdminClient", fallback = UserClientFallback.class)
+@FeignClient(name = "user-service", contextId = "userAdminClient", fallback = UserClientFallback.class)
 public interface UserClient extends UserOperations {
 
-    @GetMapping("/{id}")
-    UserShortDto getUserShort(@PathVariable("id") long id);
 }
--- core/interaction-api/src/main/java/ru/yandex/practicum/client/user/UserOperations.java
+++ core/interaction-api/src/main/java/ru/yandex/practicum/client/user/UserOperations.java
@@ -1,8 +1,10 @@
 package ru.yandex.practicum.client.user;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import ru.yandex.practicum.dto.user.UserShortDto;
 
 public interface UserOperations {
 
+    @GetMapping("/admin/users/{id}")
     UserShortDto getUser(long userId);
 }
