e5c24ec7c07ce195688b33779de4f6e560511219
f1zik <tkyskii2004@gmail.com>
2025-12-12 22:41:06 +0500

    Фикс#26

--- core/interaction-api/src/main/java/ru/yandex/practicum/client/user/UserOperations.java
+++ core/interaction-api/src/main/java/ru/yandex/practicum/client/user/UserOperations.java
@@ -1,10 +1,11 @@
 package ru.yandex.practicum.client.user;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import ru.yandex.practicum.dto.user.UserShortDto;
 
 public interface UserOperations {
 
     @GetMapping("/admin/users/{id}")
-    UserShortDto getUser(long userId);
+    UserShortDto getUser(@PathVariable("id") long userId);
 }
