46cb1ee8260d94a9c2193d1ca67556b21a9f4cfd
f1zik <tkyskii2004@gmail.com>
2025-12-16 16:04:03 +0500

    #4

--- core/event-service/src/main/java/ru/yandex/practicum/main/EventServiceApplication.java
+++ core/event-service/src/main/java/ru/yandex/practicum/main/EventServiceApplication.java
@@ -4,12 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableJpaAuditing
 @EnableFeignClients(basePackages = "ru.yandex.practicum.client")
+@ComponentScan(basePackages = {
+        "ru.yandex.practicum.main",
+        "ru.yandex.practicum.client"
+})
 public class EventServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(EventServiceApplication.class, args);
